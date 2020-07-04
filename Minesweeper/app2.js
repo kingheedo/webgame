a29220f744bb65904aaf5797a4b1947433811113
kingheedo <52102550+kingheedo@users.noreply.github.com>
2020-07-04 17:55:05 +0900

    2020-07-04
    2020-07-04

--- Minesweeper/app2.js
+++ Minesweeper/app2.js
@@ -1,8 +1,12 @@
+var tbody = document.querySelector('#table tbody');
+
+
 document.querySelector('#exec').addEventListener('click', function () {
+    tbody.innerHTML = '';
     var hor = parseInt(document.querySelector('#hor').value);
     var ver = parseInt(document.querySelector('#ver').value);
     var mine = parseInt(document.querySelector('#mine').value);
-    console.log(hor, ver, mine);
+    // console.log(hor, ver, mine);
 
     //지뢰 위치 뽑기
     var 후보군 = Array(hor * ver).fill().map(function (요소, 인덱스) {
@@ -14,12 +18,11 @@ document.querySelector('#exec').addEventListener('click', function () {
         var 이동값 = 후보군.splice(Math.floor(Math.random() * 후보군.length), 1)[0];
         셔플.push(이동값);
     }
-    console.log(셔플);
+    // console.log(셔플);
 
 
     //지뢰 테이블 만들기
     var dataset = [];
-    var tbody = document.querySelector('#table tbody');
     for (var i = 0; i < ver; i++) {
         var arr = [];
         var tr = document.createElement('tr');
@@ -27,6 +30,25 @@ document.querySelector('#exec').addEventListener('click', function () {
         for (var j = 0; j < hor; j++) {
             arr.push(1);
             var td = document.createElement('td');
+            td.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+                var 부모tbody = e.currentTarget.parentNode.parentNode;
+                var 부모tr = e.currentTarget.parentNode;
+                var 줄 = Array.prototype.indexOf.call(부모tbody.children, 부모tr); //유사 배열이기때문에 indexof 사용불가 그래서 prototype을 사용해서 indexof 사용
+                var 칸 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
+                // console.log(부모tbody, 부모tr, 줄, 칸);
+                if (e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X') {
+                    e.currentTarget.textContent = '!';
+                } else if (e.currentTarget.textContent === '!') {
+                    e.currentTarget.textContent = '?';
+                } else if (e.currentTarget.textContent === '?') {
+                    if (dataset[줄][칸] === 'X') {
+                        e.currentTarget.textContent = 'X';
+                    } else if (dataset[줄][칸] === 1) {
+                        e.currentTarget.textContent = '';
+                    }
+                }
+            });
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
@@ -36,9 +58,9 @@ document.querySelector('#exec').addEventListener('click', function () {
     for (var k = 0; k < 셔플.length; k++) {
         var 세로 = Math.floor(셔플[k] / 10);
         var 가로 = 셔플[k] % 10;
-        console.log(세로, 가로);
-        tbody.children[세로].children[가로].textContent = 'X';
+        // console.log(세로, 가로);
         dataset[세로][가로] = 'X';
+        tbody.children[세로].children[가로].textContent = 'X';
     }
     console.log(dataset);
 
