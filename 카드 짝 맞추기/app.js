e71aed2b449e04b8930cacd741e71adb0560a364
kingheedo <52102550+kingheedo@users.noreply.github.com>
2020-07-11 18:05:49 +0900

    2020-07-11
    2020-07-11

--- 카드 짝 맞추기/app.js
+++ 카드 짝 맞추기/app.js
@@ -1,49 +1,107 @@
 var 가로 = 4;
 var 세로 = 3;
-var 색깔후보 = ['red', 'red', 'orange', 'orange', 'green', 'green', 'yellow', 'yellow', 'white', 'white', 'pink', 'pink'];
+var 색깔들 = [
+  "red",
+  "red",
+  "orange",
+  "orange",
+  "green",
+  "green",
+  "yellow",
+  "yellow",
+  "white",
+  "white",
+  "pink",
+  "pink",
+];
 var 색깔 = [];
-for (var i = 0; 색깔후보.length > 0; i++) {
-    색깔 = 색깔.concat(색깔후보.splice(Math.floor(Math.random() * 색깔후보.length), 1));
+var 색깔후보 = 색깔들.slice();
+var 클릭플래그 = true;
+var 클릭카드 = [];
+var 완성카드 = [];
+var 시작시간;
+
+function 셔플() {
+  for (var i = 0; 색깔후보.length > 0; i++) {
+    색깔 = 색깔.concat(
+      색깔후보.splice(Math.floor(Math.random() * 색깔후보.length), 1)
+    );
+  }
 }
-console.log(색깔);
-var 중단플래그 = true;
 
 function 카드세팅(가로, 세로) {
-    중단플래그 = false;
-    for (var i = 0; i < 가로 * 세로; i++) {
-        var card = document.createElement('div');
-        card.className = 'card';
-        var cardInner = document.createElement('div');
-        cardInner.className = 'card-inner';
-        var cardFront = document.createElement('div');
-        cardFront.className = 'card-front'
-        var cardBack = document.createElement('div');
-        cardBack.className = 'card-back';
-        cardBack.style.backgroundColor = 색깔[i];
-        cardInner.appendChild(cardFront);
-        cardInner.appendChild(cardBack);
-        card.appendChild(cardInner);
+  클릭플래그 = false;
+  for (var i = 0; i < 가로 * 세로; i++) {
+    var card = document.createElement("div");
+    card.className = "card";
+    var cardInner = document.createElement("div");
+    cardInner.className = "card-inner";
+    var cardFront = document.createElement("div");
+    cardFront.className = "card-front";
+    var cardBack = document.createElement("div");
+    cardBack.className = "card-back";
+    cardBack.style.backgroundColor = 색깔[i];
+    cardInner.appendChild(cardFront);
+    cardInner.appendChild(cardBack);
+    card.appendChild(cardInner);
 
-        (function (c) { //비동기 클로저문제로인해 즉시실행함수로만듬
-            card.addEventListener('click', function () {
-                if (중단플래그) {
-                    c.classList.toggle('flipped');
-                }
-            });
-        })(card);
-        document.body.appendChild(card);
-    }
-    document.querySelectorAll('.card').forEach(function (card, index) {
-        setTimeout(() => {
-            card.classList.add('flipped');
-
-        }, 1000 + 100 * index);
+    (function (c) {
+      //비동기 클로저문제로인해 즉시실행함수로만듬
+      card.addEventListener("click", function () {
+        if (클릭플래그 && !완성카드.includes(c)) {
+          c.classList.toggle("flipped");
+          클릭카드.push(c);
+          if (클릭카드.length === 2) {
+            if (
+              클릭카드[0].querySelector(".card-back").style.backgroundColor ===
+              클릭카드[1].querySelector(".card-back").style.backgroundColor
+            ) {
+              완성카드.push(클릭카드[0]);
+              완성카드.push(클릭카드[1]);
+              클릭카드 = [];
+              if (완성카드.length === 가로 * 세로) {
+                var 끝시간 = new Date();
+                alert(
+                  "축하합니다! 성공!" +
+                    (끝시간 - 시작시간) / 1000 +
+                    "초 걸렸습니다."
+                );
+                document.querySelector("#wrapper").innerHTML = ""; //초기화
+                색깔 = [];
+                색깔후보 = 색깔들.slice(); //문자열, 숫자,불린은 복사가 되고 객체,배열,함수는 참조가 된다 .
+                완성카드 = [];
+                시작시간 = null;
+                셔플();
+                카드세팅(가로, 세로);
+              }
+            } else {
+              //두 카드의 색깔이 다르면
+              클릭플래그 = false;
+              setTimeout(function () {
+                클릭카드[0].classList.remove("flipped");
+                클릭카드[1].classList.remove("flipped");
+                클릭플래그 = true;
+                클릭카드 = [];
+              }, 1000);
+            }
+          }
+        }
+      });
+    })(card);
+    document.querySelector("#wrapper").appendChild(card);
+  }
+  document.querySelectorAll(".card").forEach(function (card, index) {
+    setTimeout(function () {
+      card.classList.add("flipped");
+    }, 1000 + 100 * index);
+  });
+  setTimeout(function () {
+    document.querySelectorAll(".card").forEach(function (card, index) {
+      card.classList.remove("flipped");
     });
-    setTimeout(() => {
-        document.querySelectorAll('.card').forEach(function (card, index) {
-            card.classList.remove('flipped');
-        });
-        중단플래그 = true;
-    }, 5000);
+    클릭플래그 = true;
+    시작시간 = new Date();
+  }, 5000);
 }
+셔플();
 카드세팅(가로, 세로);
